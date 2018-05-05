3d0d726deeb40f1552b0b67d6a88f13be4a69ded
TheWorld1 <37362447+TheWorld1@users.noreply.github.com>
2018-05-05 13:07:40 +0800

    Changes made in Photo Gallery

--- assign2/script/enhancements.js
+++ assign2/script/enhancements.js
@@ -62,30 +62,28 @@ var x = setInterval(function() {
 
 
 //3rd Enhancement
-//Photo Gallery//
+// Get the elements with class="column"
 var elements = document.getElementsByClassName("column");
+// Declare a loop variable
 var i;
-
 // Two images side by side
 function two() {
-	for (i = 0; i < elements.length; i++) {
-		elements[i].style.flex = "50%";
-	}
+    for (i = 0; i < elements.length; i++) {
+        elements[i].style.msFlex = "50%";  // IE10
+        elements[i].style.flex = "50%";
+    }
+}
+// Four images side by side
+function four() {
+    for (i = 0; i < elements.length; i++) {
+        elements[i].style.msFlex = "25%";  // IE10
+        elements[i].style.flex = "25%";
+    }
 }
 
-// Add active class to the current button (highlight it)
-var header = document.getElementById("myHeader");
-var btns = document.getElementsByClassName("btn");
-for (var i = 0; i < btns.length; i++) {
-  btns[i].addEventListener("click", function() {
-	var current = document.getElementsByClassName("active");
-	current[0].className = current[0].className.replace(" active", "");
-	this.className += " active";
-  });
-}
-		
-		
-		
+
+
+	
 		
 		
 //4th Enhancement
